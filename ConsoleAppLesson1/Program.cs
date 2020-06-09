76dca79de9873e1529aedec8d1eb5b606617fd27
kareemcontreras <66689752+kareemcontreras@users.noreply.github.com>
2020-06-09 13:46:12 -0500

    Add project files.

---
+++ ConsoleAppLesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Working with Github");
+        }
+    }
+}
